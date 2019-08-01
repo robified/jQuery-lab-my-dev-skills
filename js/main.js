36ed07ef9f5eb5aabb22e0107805e8e4870b7901
Robin <daorob@gmail.com>
2019-07-31 20:42:38 -0500

    Update files.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-alert("Herro!");
+$("#add").on("click", function() {
+    let $input = $('input').val();
+    $('.main').append(`<div id="xBox"><span id="square">X</span>${$input}</div>`);
+    $('input').val("");
+})
+
+$(".main").on("click", function() {
+    let $listOfText = $('#xBox');
+    $listOfText.remove();
+});
